c6a9969a062ef2fa3d2c11de3ea833e600d9ab71
kalexo-n <kalexosorio@gmail.com>
2026-03-25 18:35:35 -0500

    Add React components to components.jsx

---
+++ components.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+
+const FloralSVG = () => {
+    return <div>Floral SVG Component</div>;
+};
+
+const Hero = () => {
+    return <div>Hero Component</div>;
+};
+
+const Countdown = () => {
+    return <div>Countdown Component</div>;
+};
+
+const Mensaje = () => {
+    return <div>Mensaje Component</div>;
+};
+
+const FotoPareja = () => {
+    return <div>Foto Pareja Component</div>;
+};
+
+const Itinerario = () => {
+    return <div>Itinerario Component</div>;
+};
+
+const Lugares = () => {
+    return <div>Lugares Component</div>;
+};
+
+const Galeria = () => {
+    return <div>Galeria Component</div>;
+};
+
+const Versiculo = () => {
+    return <div>Versiculo Component</div>;
+};
+
+const Fiesta = () => {
+    return <div>Fiesta Component</div>;
+};
+
+const Hashtag = () => {
+    return <div>Hashtag Component</div>;
+};
+
+const Recomendaciones = () => {
+    return <div>Recomendaciones Component</div>;
+};
+
+const Confirmar = () => {
+    return <div>Confirmar Component</div>;
+};
+
+const Footer = () => {
+    return <div>Footer Component</div>;
+};
+
+const AccesoDenegado = () => {
+    return <div>Acceso Denegado Component</div>;
+};
+
+const Cargando = () => {
+    return <div>Cargando Component</div>;
+};
+
+export {
+    FloralSVG,
+    Hero,
+    Countdown,
+    Mensaje,
+    FotoPareja,
+    Itinerario,
+    Lugares,
+    Galeria,
+    Versiculo,
+    Fiesta,
+    Hashtag,
+    Recomendaciones,
+    Confirmar,
+    Footer,
+    AccesoDenegado,
+    Cargando
+};
